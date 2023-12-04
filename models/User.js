05e67f837b00444965d87f43db82ddf1d09adb08
reasonablep <140934359+reasonablep@users.noreply.github.com>
2023-12-03 21:33:43 -0800

    Created handlebars files, added code for homepage and main

--- models/User.js
+++ models/User.js
@@ -1,3 +1,54 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require ('bcrypt');
 const sequelize = require ('../config/connection');
+
+class User extends Model {
+    checkPassword(userPassword) {
+        return bcrypt.compareSync(userPassword, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+
+    
+    },
+},
+
+{
+
+hooks: {
+    beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+    },
+},
+
+sequelize,
+timestamps: false,
+freezeTableName: true,
+underscored: true,
+modelName: 'user'
+}
+);
+
+module.exports = User;
